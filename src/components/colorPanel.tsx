ecc95e448ceaa75567cbd279611083131fec923f
ClarkeSwiedler <Clarke@swiedler.com>
2019-11-16 12:59:14 -0500

    add colorpanel

---
+++ src/components/colorPanel.tsx
@@ -0,0 +1,9 @@
+import React, {useEffect, useRef} from 'react'
+
+interface ColorPanelProps {}
+
+const ColorPanel = (props: ColorPanelProps) => {
+  return <canvas style={{width: '100px', height: '100px'}} />
+}
+
+export default ColorPanel
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,24 @@
 import React from 'react'
 import Layout from '../components/layout'
+import {styled} from '../styles/theme'
+import ColorPanel from '../components/colorPanel'
+
+const CanvasHost = styled.div`
+  width: 100%;
+  height: 400px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`
 
 const index = () => {
   return (
     <Layout>
-      <article>Hello there.</article>
+      <article>
+        <CanvasHost>
+          <ColorPanel />
+        </CanvasHost>
+      </article>
     </Layout>
   )
 }
